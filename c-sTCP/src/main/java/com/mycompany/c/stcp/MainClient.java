274d13854e0100d7007e8cee8f72f26a61c78377
Utente <Utente@DESKTOP-IAQ07QI.Home>
2024-03-18 20:34:59 +0100

    commit "concluso" ancora qualche problema nell'esecuzione

--- c-sTCP/src/main/java/com/mycompany/c/stcp/MainClient.java
+++ c-sTCP/src/main/java/com/mycompany/c/stcp/MainClient.java
@@ -4,13 +4,53 @@
  */
 package com.mycompany.c.stcp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author Utente
  */
 public class MainClient {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, SocketException, IOException {
+        int portaServer = 6789; //porta del server
+        InetAddress IPServer = InetAddress.getByName("localhost");
+        
+        byte[] bufferOUT = new byte[1024]; //buffer du spedizione
+        byte[] bufferIN = new byte[1024]; //buffer di ricezione
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        
+        //creazione del socket
+        DatagramSocket clientSocket = new DatagramSocket();
+        System.out.println("---IL CLIENT E' PRONTO--- *inserisci la stringa da mettere in maiuscolo* ");
+        
+        //preparazione del messaggio da inviare
+        String daSpedire = input.readLine();
+        bufferOUT = daSpedire.getBytes();
+        
+        //trasmissione del messaggio al server
+        DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPServer, portaServer);
+        clientSocket.send(sendPacket);
+        
+        //ci prepareiamo alla ricezione
+        DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
+        clientSocket.receive(receivePacket);
+        String ricevuto = new String(receivePacket.getData());
+        
+        //elaborazione dei dati ricevuti
+        int numCaratteri = receivePacket.getLength();
+        System.out.println("---LA RISPOSTA DAL SERVER E'---     " + ricevuto);
+        
+        //termine elaborazione
+        clientSocket.close();
         
     }
 }
--- c-sTCP/src/main/java/com/mycompany/c/stcp/MainServer.java
+++ c-sTCP/src/main/java/com/mycompany/c/stcp/MainServer.java
@@ -4,12 +4,55 @@
  */
 package com.mycompany.c.stcp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
 /**
  *
  * @author Utente
  */
 public class MainServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException, IOException {
+        DatagramSocket serverSocket = new DatagramSocket(6789);
+        boolean attivo = true;
+        byte[] bufferIN = new byte[1024];
+        byte[] bufferOUT = new byte[1024];
         
-    }
+        System.out.println("---IL SERVER E' AVVIATO---");
+        while(attivo);   
+              
+        DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length); 
+        
+        //attesa della ricezione
+        serverSocket.receive(receivePacket);
+        
+        //analisi del pacchetto ricevuto
+        String ricevuto = new String(receivePacket.getData());
+        int numCaratteri = receivePacket.getLength();
+        System.out.println("---IL MESSAGGIO RICEVUTO E'---     " + ricevuto);
+        
+        //riconoscimento dei parametri del socket del client
+        InetAddress IPClient = receivePacket.getAddress();
+        int portaClient = receivePacket.getPort();
+        
+        //preparo il dato da spedire
+        String daSpedire = ricevuto.toUpperCase();
+        bufferOUT = daSpedire.getBytes();
+        
+        //invio del datagramma
+        DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, IPClient, portaClient);
+        serverSocket.send(sendPacket);
+        
+        //controllo del termine dell'esecuzione
+        if (ricevuto.equals("fine")) {
+            System.out.println("---SERVER IN CHIUSURA---");
+            attivo=false;
+        }
+        serverSocket.close(); 
+    }   
 }
