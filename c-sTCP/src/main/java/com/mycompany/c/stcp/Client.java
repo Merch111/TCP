9952f628f64ecca4bd7c28e1c069930b6ec1b9e9
Utente <Utente@DESKTOP-IAQ07QI.Home>
2024-03-18 19:19:10 +0100

    Merge origin/main into main
    Conflicts:
    	README.md
    	src/main/java/com/mycompany/granprixgruppo5/Gestore.java
    	src/main/java/com/mycompany/granprixgruppo5/Giocatore.java
    	src/main/java/com/mycompany/granprixgruppo5/GiudiceDiGara.java
    	src/main/java/com/mycompany/granprixgruppo5/GranPrixGruppo5.java
    	target/classes/com/mycompany/granprixgruppo5/GranPrixGruppo5.class

---
+++ c-sTCP/src/main/java/com/mycompany/c/stcp/Client.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.c.stcp;
+
+/**
+ *
+ * @author Utente
+ */
+public class Client {
+    String nome;
+    String colore;
+    
+    public Client(String nomeDefault, String coloreDefault){
+        
+    }
+    
+    void connetti(String nomeServer, int portaServer){
+        
+    }
+    
+    void scrivi() {
+        
+    }
+    
+    void leggi() {
+        
+    }
+    
+    void chiudi() {
+        
+    }
+}
---
+++ c-sTCP/src/main/java/com/mycompany/c/stcp/MainClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.c.stcp;
+
+/**
+ *
+ * @author Utente
+ */
+public class MainClient {
+    
+    public static void main(String[] args) {
+        
+    }
+}
